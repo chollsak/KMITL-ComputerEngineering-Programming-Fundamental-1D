6ba5dbc321a6fc4c0da588f701d29fc7f2facd69
chollsakn <koonnew8688.cookie@gmail.com>
2022-08-04 09:19:08 +0700

    add : ex1 files

---
+++ ex1(2).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    
+    double a,b;
+    scanf("%lf %lf",&a,&b);
+    printf("%.2f",a+b);
+    printf("%.2f",a-b);
+    printf("%.2f",a*b);
+    printf("%.2f",a/b);
+
+    return 0;
+}
