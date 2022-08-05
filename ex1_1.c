92cf84e2c4bc3788501280829443b713e61a72f5
Chollasak Anuwareephong l Choll <83394467+chollsakn@users.noreply.github.com>
2022-08-05 22:33:09 +0700

    Update ex1_1.c

--- ex1_1.c
+++ ex1_1.c
@@ -4,7 +4,7 @@ int main()
 {
   int n[2];
   scanf("%d %d", &n[0], &n[1]);
-  printf("%d\n%d\n%d\n%d", n[0] + n[1], n[0] - n[1], n[0] * n[1], n[0] / n[1]);
+  printf("%d %d %d %d", n[0] + n[1], n[0] - n[1], n[0] * n[1], n[0] / n[1]);
   return 0;
 
 }
