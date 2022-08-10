80bac5402e7b77740f8e4bc0da390b5a5c7abf6c
Chollasak Anuwareephong l Choll <83394467+chollsakn@users.noreply.github.com>
2022-08-10 15:05:49 +0700

    Add files via upload

---
+++ ex21_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    
+    int n,i,j;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    while(j<n){
+        for(i=0;i<n;i++){
+            printf("*");
+    }
+    printf("\n");
+    j++;
+    }
+
+    return 0;
+}
